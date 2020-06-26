a60577a3b65b4dbc1951ebf7d298092ee03ef9d4
Pieterjan De Clippel <pieterjandeclippel@hotmail.com>
2020-06-26 15:59:35 +0200

    Add project files.

---
+++ WpfApp2/MainVM.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows.Input;
+
+namespace WpfApp2
+{
+    public class MainVM : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+        private MintPlayer.ObservableCollection.ObservableCollection<Item> items = new MintPlayer.ObservableCollection.ObservableCollection<Item>();
+        public MintPlayer.ObservableCollection.ObservableCollection<Item> Items
+        {
+            get { return items; }
+            set
+            {
+                items = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private ICommand addItemsCommand;
+        public ICommand AddItemsCommand
+        {
+            get
+            {
+                if (addItemsCommand == null)
+                    addItemsCommand = new RelayCommand(OnAddItems, CanAddItems);
+                return addItemsCommand;
+            }
+        }
+
+        private void OnAddItems(object obj)
+        {
+            Items.AddRange(new[]
+            {
+                new Item { Id = "1", Text = "Pieterjan", Description = "Pieterjan" },
+                new Item { Id = "1", Text = "Pieterjan", Description = "Pieterjan" },
+                new Item { Id = "1", Text = "Pieterjan", Description = "Pieterjan" },
+                new Item { Id = "1", Text = "Pieterjan", Description = "Pieterjan" }
+            });
+        }
+
+        private bool CanAddItems(object arg)
+        {
+            return true;
+        }
+    }
+}
---
+++ WpfApp2/RelayCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace WpfApp2
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Action<object> execute;
+        private readonly Func<object, bool> canExecute;
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            execute(parameter);
+        }
+
+        public event EventHandler CanExecuteChanged;
+        public void ChangeCanExecute()
+        {
+            if (CanExecuteChanged != null)
+                CanExecuteChanged(this, EventArgs.Empty);
+        }
+    }
+}
